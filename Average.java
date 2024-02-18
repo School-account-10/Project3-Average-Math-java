c123396b8238d10f40daf43d59eb6e23a206f625
じゅういち <160393316+School-account-10@users.noreply.github.com>
2024-02-18 10:38:08 +0000

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,11 @@
+public class Average {
+    public static void main(String[]args) {
+        System.out.println("Compute using same Data Type"); 
+        double average=0;
+        double quiz_1=40, quiz_2=3;
+        average = (quiz_1+quiz_2)/2;
+        System.out.println("The average of " + quiz_1 + " and "+ quiz_2 + " is " + average);
+    }
+}
+
+
